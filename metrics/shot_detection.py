774f4d2abc5ec3b899dd0cb85d6bf089115ae216
auroua <auroua@yeah.net>
2015-10-13 15:54:59 +0800

    add the get file list method

--- metrics/shot_detection.py
+++ metrics/shot_detection.py
@@ -1,2 +1,29 @@
+#encoding:UTF-8
 __author__ = 'auroua'
 
+import numpy as np
+import cv2
+import os
+
+# /home/auroua/workspace/PycharmProjects/data/N20040103G/
+
+def getImg(url):
+    pass
+
+def getFiles(path):
+    filelist = []
+    FileNames=os.listdir(path)
+    if (len(FileNames)>0):
+       for fn in FileNames:
+            fullfilename=os.path.join(path,fn)
+            filelist.append(fullfilename)
+
+    #对文件名排序
+    if (len(filelist)>0):
+        filelist.sort()
+
+    return filelist
+
+if __name__=='__main__':
+    filenames = getFiles('/home/auroua/workspace/PycharmProjects/data/N20040103G')
+    print filenames
