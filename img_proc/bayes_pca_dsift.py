82a56521b05845db8bdb3c1482b0ba1efac2c43a
aurora <auroua@yeah.net>
2015-12-14 17:54:34 +0800

    bayes v0.1

--- img_proc/bayes_pca_dsift.py
+++ img_proc/bayes_pca_dsift.py
@@ -1,3 +1,24 @@
 import pca
+import dsift_test as dsift
+import sift
+import numpy as np
+import bayes
 
-V,S,m = pca.pca(features)
+
+if __name__ == '__main__':
+    url1 = '/home/aurora/hdd/workspace/PycharmProjects/data/pcv_img/gesture/train'
+    url2 = '/home/aurora/hdd/workspace/PycharmProjects/data/pcv_img/gesture/test'
+    featurelist, labels = dsift.read_gesture_features_labels(url1)
+    test_featurelist, test_labels = dsift.read_gesture_features_labels(url2)
+    V,S,m = pca.pca(featurelist)
+    classnames = ['A', 'B', 'C', 'F', 'P', 'V']
+    V = V[:50]
+    features = np.array([np.dot(V, f-m) for f in featurelist])
+    test_features = np.array([np.dot(V, f-m) for f in test_featurelist])
+    bc = bayes.BayesClassifier()
+    blist = [features[np.where(labels == c)[0]] for c in classnames]
+    bc.train(blist, classnames)
+    res = bc.classify(test_features)[0]
+    acc = np.sum((res==test_labels)*1.0)/len(test_labels)
+    print acc
+    dsift.print_confusion(res, test_labels, classnames)
